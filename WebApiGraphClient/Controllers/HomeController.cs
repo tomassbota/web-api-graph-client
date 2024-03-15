768852250ac8773e32814fd14c8da75502d5d5b1
Tomás Bota <tbota@mainhub.pt>
2024-03-15 23:48:32 +0000

    Fixed bug with injected graph client

---
+++ WebApiGraphClient/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+
+namespace WebApiGraphClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private GraphServiceClient _graphServiceClient;
+
+        public HomeController(GraphServiceClient graphServiceClient)
+        {
+            _graphServiceClient = graphServiceClient;
+        }
+
+        [HttpGet("/getUsers")]
+        public async Task<IActionResult> GetUser()
+        {
+            var users = await _graphServiceClient.Users.Request().WithAppOnly().GetAsync();
+
+            return Ok(users);
+        }
+    }
+}
--- WebApiGraphClient/Program.cs
+++ WebApiGraphClient/Program.cs
@@ -12,12 +12,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 builder.Services.AddAuthorization();
 
-//builder.Services.AddAuthorization(options =>
-//{
-//    // By default, all incoming requests will be authorized according to the default policy
-//    options.FallbackPolicy = options.DefaultPolicy;
-//});
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
